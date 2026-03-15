aa713888e3cba33c27327898af59078016011625
Link Googly LLP <linkgoogly.services@gmail.com>
2026-03-15 12:57:12 +0530

    First
    First

---
+++ admin/assets/js/main.js
@@ -0,0 +1,10 @@
+let list  = document.querySelector(".navigation li");
+
+function activeLink(){
+    list.forEach((item) =>{
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+};
+
+list.forEach((item) => item.addEventListener("mousehover", activeLink));
